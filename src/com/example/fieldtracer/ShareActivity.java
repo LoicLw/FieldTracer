2a060afdce6e2d80713d57d1cd92a7146820eb7e
LoicLWRT <loic.lwrt@fake.address>
2013-04-01 18:36:35 +1030

    Added sharing with serval activity and small map selector in settings

--- src/com/example/fieldtracer/ShareActivity.java
+++ src/com/example/fieldtracer/ShareActivity.java
@@ -1,14 +1,37 @@
 package com.example.fieldtracer;
 
-import android.os.Bundle;
+import java.io.File;
+import java.io.FilenameFilter;
+
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.annotation.TargetApi;
-import android.os.Build;
+import android.view.View;
+import android.widget.TextView;
+
+
 
 public class ShareActivity extends Activity {
+
+//In an Activity
+private String[] mFileList;
+private File mPath = new File(Environment.getExternalStorageDirectory(),"/_FieldTracer/");
+private String mChosenFile;
+private static final String FTYPE = ""; //if we want an extension filter
+private static final int DIALOG_LOAD_FILE = 1000;	
+
+private TextView fileChoosed = null;  
+private static final String TAG = "Debug";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +39,7 @@ public class ShareActivity extends Activity {
 		setContentView(R.layout.activity_share);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		fileChoosed = (TextView) findViewById(R.id.textView1);   
 	}
 
 	/**
@@ -51,5 +75,65 @@ public class ShareActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	private void loadFileList() {
+	    try {
+	        mPath.mkdirs();
+	    }
+	    catch(SecurityException e) {
+	        Log.e(TAG, "unable to write on the sd card " + e.toString());
+	    }
+	    if(mPath.exists()) {
+	        FilenameFilter filter = new FilenameFilter() {
+	            public boolean accept(File dir, String filename) {
+	                File sel = new File(dir, filename);
+	                return filename.contains(FTYPE) || sel.isDirectory();
+	            }
+	        };
+	        mFileList = mPath.list(filter);
+	    }
+	    else {
+	        mFileList= new String[0];
+	    }
+	}
 
+	protected Dialog onCreateDialog(int id) {
+	    Dialog dialog = null;
+	    AlertDialog.Builder builder = new Builder(this);
+
+	    switch(id) {
+	        case DIALOG_LOAD_FILE:
+	            builder.setTitle("Choose your file");
+	            if(mFileList == null) {
+	                Log.e(TAG, "Showing file picker before loading the file list");
+	                dialog = builder.create();
+	                return dialog;
+	            }
+	            builder.setItems(mFileList, new DialogInterface.OnClickListener() {
+	                public void onClick(DialogInterface dialog, int which) {
+	                    mChosenFile = mFileList[which];
+	                    //you can do stuff with the file here too
+	                    fileChoosed.setText(mPath + "/" + mChosenFile);   
+	                }
+	            });
+	            break;
+	    }
+	    dialog = builder.show();
+	    return dialog;
+	}
+	
+	public void ButtonOnSelectFile(View v){
+		// create alert dialog
+		loadFileList();
+		this.onCreateDialog(DIALOG_LOAD_FILE);
+	}
+
+	public void ButtonOnShareFile(View v){
+		Rhizome.addFile(getBaseContext(),mPath + "/" + mChosenFile);
+		 Builder dialog= new AlertDialog.Builder(this);
+         dialog.setMessage("File added to Rhizome store");
+         dialog.setPositiveButton("OK", null);
+         dialog.show();
+    }
+	
 }
